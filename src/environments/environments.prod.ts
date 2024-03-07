b6d50d8f581c32c39d17778b6ffd3f67dbf3d20e
ZeyarHtet <zeyarhtet.zyh@gmail.com>
2024-03-07 15:32:24 +0630

    test

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
-    production: true,
-    firebaseConfig: {
-        apiKey: "AIzaSyDxDetSXchpWak_wcsOWHR2UFFNkTeLtfc",
-        authDomain: "http-client-8c56c.firebaseapp.com",
-        databaseURL: "https://http-client-8c56c-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "http-client-8c56c",
-        storageBucket: "http-client-8c56c.appspot.com",
-        messagingSenderId: "1012888833559",
-        appId: "1:1012888833559:web:b18b155641230e9e54d652",
-        measurementId: "G-NQR2KF2502"
-    }
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDuSutnXi8XCyYAOsSDqJdFKtQJ4gVQO6U",
+    authDomain: "http-client-d5dcd.firebaseapp.com",
+    databaseURL: "https://http-client-d5dcd-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "http-client-d5dcd",
+    storageBucket: "http-client-d5dcd.appspot.com",
+    messagingSenderId: "396009169921",
+    appId: "1:396009169921:web:4959a36778420f175a8c00",
+    measurementId: "G-7F682RP7CP"
+  },
+  
 };
